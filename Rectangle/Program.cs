123eda27293a81310ed547a47c616a2dc25dc0c6
Mykola Kovshar <knikolay20031230@gmail.com>
2022-09-24 17:20:11 +0300

    Add Rectangle project

---
+++ Rectangle/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rectangle
+{
+    internal class Program
+    {
+        static void ThrowExeption()
+        {
+            throw new ArgumentException("Параметр не відповідає умовам");
+        }
+
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.InputEncoding = Encoding.UTF8;
+            double width, height;
+            Console.WriteLine("Введіть ширину");
+            if(!double.TryParse(Console.ReadLine(), out width) || width < 0)
+            {
+                ThrowExeption();
+            }
+            Console.WriteLine("Введіть висоту");
+            if(!double.TryParse(Console.ReadLine(), out height) || height < 0)
+            {
+                ThrowExeption();
+            }
+
+            Rectangle rectangle = new Rectangle(width, height);
+
+            Console.WriteLine($"Площа: {rectangle.Area}");
+            Console.WriteLine($"Периметр: {rectangle.Perimeter}");
+        }
+    }
+}
---
+++ Rectangle/Rectangle.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rectangle
+{
+    public class Rectangle
+    {
+        private double _width;
+        private double _height;
+
+        public Rectangle(double width, double height)
+        {
+            this._width = width;
+            this._height = height;
+        }
+
+        private double AreaCalculator() => _width * _height;
+        private double PerimeterCalculator() => 2 * (_width + _height);
+
+        public double Area => AreaCalculator();
+        public double Perimeter => PerimeterCalculator();
+    }
+}

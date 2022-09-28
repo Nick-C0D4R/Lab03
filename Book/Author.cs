5826bbfe715d625eecdd519b2f9ae0a0e14a89ad
Mykola Kovshar <knikolay20031230@gmail.com>
2022-09-28 23:17:52 +0300

    Add Book project

---
+++ Book/Author.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Book
+{
+    public class Author
+    {
+        public string Name { get; set; }
+
+        public void Show() => Console.WriteLine(Name);
+    }
+}
---
+++ Book/Title.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Book
+{
+    public class Title
+    {
+        public string Content { get; set; }
+
+        public void Show() => Console.WriteLine(Content);
+    }
+}

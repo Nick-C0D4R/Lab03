6d2670af94a7022b93424bb23180899fbded83ff
Mykola Kovshar <knikolay20031230@gmail.com>
2022-09-28 23:45:25 +0300

    Add Invoice project

---
+++ Invoice/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Invoice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Invoice invoice = new Invoice(1, "provider", "customer", "article", 33);
+            Console.WriteLine(invoice.GetSum());
+            Console.WriteLine(invoice.GetSum(false));
+        }
+    }
+}

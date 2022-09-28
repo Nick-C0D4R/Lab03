b16a5ec8524f98a1bc162d6ac25768078689df05
Mykola Kovshar <knikolay20031230@gmail.com>
2022-09-28 23:36:36 +0300

    Update Point project

--- Point/Program.cs
+++ Point/Program.cs
@@ -10,6 +10,8 @@ namespace Point
     {
         static void Main(string[] args)
         {
+            Figure figure = new Figure(new Point(1, 1, "A"), new Point(4, 1, "B"), new Point(4, 5, "C"));
+            Console.WriteLine(figure.PerimeterCalculator());
         }
     }
 }
